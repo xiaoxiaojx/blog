e5e7d32fdeb98e6b05baea6d063d51ea38b3d3f6
kaiduo <kaiduo@pinduoduo.com>
2021-07-31 18:07:24 +0800

    add addons

---
+++ addons/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/addon/hello.o := c++ -o Release/obj.target/addon/hello.o ../hello.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node -I/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/src -I/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/deps/openssl/config -I/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/deps/openssl/openssl/include -I/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/deps/uv/include -I/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/deps/zlib -I/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/deps/v8/include  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++14 -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c
+Release/obj.target/addon/hello.o: ../hello.cc \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/node.h \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8.h \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/cppgc/common.h \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8config.h \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8-internal.h \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8-version.h \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8-platform.h \
+  /Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/node_version.h
+../hello.cc:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/node.h:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8.h:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/cppgc/common.h:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8config.h:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8-internal.h:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8-version.h:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/v8-platform.h:
+/Users/xiaojiaxin/Library/Caches/node-gyp/16.5.0/include/node/node_version.h:
