996b1df2ce5abd912b9ff2d17eeb08b8ea32e3eb
kaiduo <kaiduo@pinduoduo.com>
2021-07-31 23:53:51 +0800

    add demo

---
+++ demo_NODE_MODULE_INITIALIZER.cc
@@ -0,0 +1,17 @@
+#include <node.h>
+#include <v8.h>
+
+static void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(
+        isolate, "world").ToLocalChecked());
+}
+
+// Not using the full NODE_MODULE_INIT() macro here because we want to test the
+// addon loader's reaction to the FakeInit() entry point below.
+extern "C" NODE_MODULE_EXPORT void
+NODE_MODULE_INITIALIZER(v8::Local<v8::Object> exports,
+                        v8::Local<v8::Value> module,
+                        v8::Local<v8::Context> context) {
+  NODE_SET_METHOD(exports, "hello", Method);
+}
