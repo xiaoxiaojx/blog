9583173aaae5768533af47f1c68b247e5fa99e0e
kaiduo <kaiduo@pinduoduo.com>
2021-09-06 01:41:06 +0800

    add grpc

---
+++ grpc/helloworld_client_stream/greeter_client.js
@@ -0,0 +1,45 @@
+var path = require('path')
+var PROTO_PATH = path.join(__dirname, '../helloworld_client_stream.proto');
+
+var parseArgs = require('minimist');
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+function main() {
+  var argv = parseArgs(process.argv.slice(2), {
+    string: 'target'
+  });
+  var target;
+  if (argv.target) {
+    target = argv.target;
+  } else {
+    target = 'localhost:50051';
+  }
+  var client = new hello_proto.Greeter(target,
+                                       grpc.credentials.createInsecure());
+  var user;
+  if (argv._.length > 0) {
+    user = argv._[0]; 
+  } else {
+    user = 'world';
+  }
+  var call = client.sayHello((err, stats) => {
+    console.log(err, stats)
+  });
+
+  [1, 2, 3].forEach((num) => {
+    call.write({ message: "hello" + num });
+  });
+  call.end();
+}
+
+main();
---
+++ grpc/helloworld_server_stream/greeter_client.js
@@ -0,0 +1,42 @@
+var path = require('path')
+var PROTO_PATH = path.join(__dirname, '../helloworld_server_stream.proto');
+
+var parseArgs = require('minimist');
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+function main() {
+  var argv = parseArgs(process.argv.slice(2), {
+    string: 'target'
+  });
+  var target;
+  if (argv.target) {
+    target = argv.target;
+  } else {
+    target = 'localhost:50051';
+  }
+  var client = new hello_proto.Greeter(target,
+                                       grpc.credentials.createInsecure());
+  var user;
+  if (argv._.length > 0) {
+    user = argv._[0]; 
+  } else {
+    user = 'world';
+  }
+  var call = client.sayHello({name: user});
+
+  call.on('data', response => {
+    console.log('Greeting:', response.message);
+  })
+}
+
+main();
